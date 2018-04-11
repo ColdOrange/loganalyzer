43efd0f7db49d6f0d4702af78e6ea10aedbffa8d
Orange <904412343@qq.com>
2018-04-11 19:30:03 +0800

    Add 404 and 500 page

--- assets/src/pages/NotFound/index.js
+++ assets/src/pages/NotFound/index.js
@@ -1,12 +1,28 @@
 // @flow
 
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'antd';
+
+import styles from './index.css';
 
 class NotFound extends React.Component<{}> {
   render() {
     return (
-      <div>
-        <h1>404 Not Found</h1>
+      <div className={styles.exception}>
+        <div className={styles.imgBlock}>
+          <div
+            className={styles.imgEle}
+            style={{ backgroundImage: 'url(/static/images/404.svg)' }}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.h1}>404</h1>
+          <div className={styles.desc}>Page Not Found</div>
+          <Link to={'/'}>
+            <Button>Go Home</Button>
+          </Link>
+        </div>
       </div>
     );
   }
--- assets/src/pages/ServerError/index.js
+++ assets/src/pages/ServerError/index.js
@@ -6,19 +6,19 @@ import { Button } from 'antd';
 
 import styles from './index.css';
 
-class NotFound extends React.Component<{}> {
+class ServerError extends React.Component<{}> {
   render() {
     return (
       <div className={styles.exception}>
         <div className={styles.imgBlock}>
           <div
             className={styles.imgEle}
-            style={{ backgroundImage: 'url(/static/images/404.svg)' }}
+            style={{ backgroundImage: 'url(/static/images/500.svg)' }}
           />
         </div>
         <div className={styles.content}>
-          <h1 className={styles.h1}>404</h1>
-          <div className={styles.desc}>Page Not Found</div>
+          <h1 className={styles.h1}>500</h1>
+          <div className={styles.desc}>Internal Server Error</div>
           <Link to={'/'}>
             <Button>Go Home</Button>
           </Link>
@@ -28,4 +28,4 @@ class NotFound extends React.Component<{}> {
   }
 }
 
-export default NotFound;
+export default ServerError;
