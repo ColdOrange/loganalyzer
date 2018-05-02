67a7b8c63bc868727c97f4c511b6a14b5a8b810f
Orange <904412343@qq.com>
2018-05-02 09:56:35 +0800

    Fix webpack watcher

--- loganalyzer/watcher.go
+++ loganalyzer/watcher.go
@@ -11,35 +11,39 @@ import (
 	log "loganalyzer/loganalyzer/logging"
 )
 
-func StartNodeWatcher() {
+func StartWebpackWatcher() {
 	webpackPath, _ := filepath.Abs(path.Join(ProjectPath, "assets/node_modules/webpack/bin/webpack.js"))
-	log.Debugln("Webpack path:", webpackPath)
-	cmd := exec.Command("node", webpackPath, "--mode", "development", "--colors")
-	cmd.Dir, _ = filepath.Abs(path.Join(ProjectPath, "assets/")) // Webpack should exec under `assets` directory
+	log.Debugln("Resolve webpack path:", webpackPath)
+	cmd := exec.Command("node", webpackPath, "--mode", "development", "--watch", "--colors")
+	cmd.Dir, _ = filepath.Abs(path.Join(ProjectPath, "assets")) // Webpack should exec under `assets` directory
 
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		log.Fatalln("Start node watcher error:", err)
+		log.Fatalln("Start webpack watcher error:", err)
 	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatalln("Start webpack watcher error:", err)
+	}
+
 	err = cmd.Start()
 	if err != nil {
-		log.Fatalln("Start node watcher error:", err)
+		log.Fatalln("Start webpack watcher error:", err)
 	}
 	log.Infoln("Webpack start watching files")
 
-	reader := bufio.NewReader(stdout)
-	for {
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			if err != io.EOF {
-				log.Fatalln("Start node watcher error:", err)
-			}
-			break
-		}
-		fmt.Print(line)
-	}
+	go copyOutput(stdout)
+	go copyOutput(stderr)
+
 	err = cmd.Wait()
 	if err != nil {
-		log.Fatalln("Start node watcher error:", err)
+		log.Fatalln("Start webpack watcher error:", err)
+	}
+}
+
+func copyOutput(r io.Reader) {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
 	}
 }
--- main.go
+++ main.go
@@ -8,16 +8,19 @@ import (
 )
 
 func init() {
-	debug := flag.Bool("d", false, "Print debug level logs")
+	debug := flag.Bool("debug", false, "Print debug level logs")
+	dev := flag.Bool("dev", false, "Run in development mode (with webpack watcher)")
 	flag.Parse()
 	if *debug {
 		log.SetLevel("Debug")
+	}
+	if *dev {
+		go loganalyzer.StartWebpackWatcher()
 	}
 }
 
 func main() {
 	loganalyzer.Analyze()
-	//go loganalyzer.StartNodeWatcher() // TODO: fix webpack watcher
 	server := loganalyzer.NewServer("127.0.0.1:8080")
 	server.ListenAndServe()
 }
